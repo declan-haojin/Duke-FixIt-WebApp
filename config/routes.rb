1ed49140edfc409395b8bcad91ab128295e06f29
Haojin Li <haojin.li@duke.edu>
2022-05-30 15:49:44 -0400

    finish user system

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :friends
+  # get 'home/index'
+  root to:'home#index'
+  get 'home/about'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
